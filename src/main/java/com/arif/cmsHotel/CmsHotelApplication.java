98870afd196ca6f532cab37182e84da8c6f01f4f
arif-rizal1122 <rizallarif32@gmail.com>
2025-03-17 16:55:32 +0800

    first setup project

---
+++ src/main/java/com/arif/cmsHotel/CmsHotelApplication.java
@@ -0,0 +1,13 @@
+package com.arif.cmsHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arif/cmsHotel/CmsHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arif.cmsHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
