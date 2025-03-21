7fe030caad633eadc4b6d12aeefa4a2969bf28ff
arif-rizal1122 <rizallarif32@gmail.com>
2025-03-21 17:02:30 +0800

    front end: add new room functionality

---
+++ client/cmsHotel/src/components/utils/ApiFunctions.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+
+export const api = axios.create({
+    baseURL :"http://localhost:8080"
+})
+
+
+/* this function add new room  */
+export async function addRoom(photo, roomType, roomPrice) {
+    
+    const formData = new FormData();
+    formData.append("photo", photo)
+    formData.append("roomType", roomType)
+    formData.append("roomPrice", roomPrice)
+
+    const response = await api.post("/rooms/add/newRoom", formData)
+    if (response.status == 201) {
+        return true
+    } else {
+        return false
+    }
+
+}
+
+/* this function gets all room types from the database */ 
+export async function getRoomType() {
+    try{
+        const response = await api.get("/rooms/room-types")
+        return response.data
+    } catch(error) {
+        throw new Error("Error Fatching Room Type");
+    }
+}
